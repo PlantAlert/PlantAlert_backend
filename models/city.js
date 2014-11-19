e817bcbda6baddfddda07932cd39f68c26f4706d
Scott Burbidge <scottisreally@gmail.com>
2014-11-19 12:16:18 -0800

    can now retrieve from open weather.

--- models/city.js
+++ models/city.js
@@ -3,6 +3,8 @@
 
 var mongoose = require('mongoose');
 var request = require('superagent');
+var bodyParser = require('body-parser')
+var express = require('express')
 
 var citySchema = mongoose.Schema({
   cityName: String, //example: Seattle,wa
@@ -15,14 +17,14 @@ citySchema.methods.pullCities = function(){
   this.model('City').find({"cityName": "Seattle,wa"}, function (err, data) {
     if (err) return console.log('DB city get all city error.'); console.log(data);
     // var parsedData = JSON.parse(data.text);
-    var cities = (data.cityName);
-    console.log(cities)
+    var cities = (data[0].cityName);
+    console.log(data[0].cityName)
 
     var cityCall = [];
     data.forEach(function(city){
       var tempParse;
       var temp;
-      var cityUrl = 'api.openweathermap.org/data/2.5/forecast/daily?q=' + city + '&cnt=3&units=imperial&APIID=20e5bcdd87db0f48d21c0e8d85d30048&mode=json';
+      var cityUrl = 'api.openweathermap.org/data/2.5/forecast/daily?q=' + city.cityName + '&cnt=3&units=imperial&APIID=20e5bcdd87db0f48d21c0e8d85d30048&mode=json';
       console.log(2)
       request
         .get(cityUrl)
@@ -30,12 +32,13 @@ citySchema.methods.pullCities = function(){
           console.log(cityData)
           if (err) console.log('there was an error');
           tempParse = JSON.parse(cityData.text);
-          temp = res.json(tempParse.list[2].temp.night);
+          temp = (tempParse.list[2].temp.night);
           return temp;
         });
         console.log(temp)
       if (temp <= 32){
         cityCall.push(city.users);
+        console.log(cityCall)
       }
 
       return cityCall;
